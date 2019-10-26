7662376061e08b60d856cf8b5fef4feca86eb522
Caio <cpeluti@gmail.com>
2019-10-26 16:03:05 -0300

    adicionado função que imprime o tabuleiro

---
+++ main.s
@@ -0,0 +1,27 @@
+.data
+.include "tabuleiro.data"
+
+.text
+
+
+
+
+
+
+
+#FUNCOES
+IMPRIME_TABULEIRO: #funcao para imprime tabuleiro no display 0
+# Utiliza s1 para carregar o tabuleiro
+
+FORA:	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
+	li t2,0xFF012C00	# endereco final 
+	la s1,tabuleiro		# endereço dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
+LOOP1: 	beq t1,t2,FIM	# Se for o último endereço então sai do loop
+	lw t3,0(s1)		# le um conjunto de 4 pixels : word
+	sw t3,0(t1)		# escreve a word na memória VGA
+	addi t1,t1,4		# soma 4 ao endereço
+	addi s1,s1,4
+	j LOOP1			# volta a verificar
+FIM:	li a7,10		# syscall de exit
+	ecall
